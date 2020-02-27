1624c0734ffbeb77470a29c95db4ff64b8f40116
wifilittt <abdulfathhashi@wifilittts-MacBook-Air.local>
2020-02-27 12:36:23 -0800

    feb27

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.post('/upload', (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
